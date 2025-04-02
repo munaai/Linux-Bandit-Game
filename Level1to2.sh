a72c1f6e20288ff1d97ef8424673e4367c8406d4
munaibrahimn <munaa97@gmail.com>
2025-04-02 13:55:24 +0100

    # added for all files and Level 2 complete

---
+++ Level1to2.sh
@@ -0,0 +1,5 @@
+# Connect to the Bandit Level 0 server
+ssh bandit1@bandit.labs.overthewire.org -p 2220
+
+# Command used after logging in
+cat ./-
---
+++ Level2to3.sh
@@ -0,0 +1,4 @@
+# Connect to the Bandit Level 0 server
+ssh bandit2@bandit.labs.overthewire.org -p 2220
+
+# Command used after logging in
